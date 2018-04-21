58fd7a8beb19bc1dcb51ffb5c569af9b232884aa
Sylvain Glaize <mokona@puupuu.org>
2018-04-21 23:05:51 +0200

    Shaking Title for GameOver page

--- gameover.cpp
+++ gameover.cpp
@@ -4,23 +4,56 @@
 #include "gametitle.h"
 
 #include <Gamebuino-Meta.h>
+
+#undef min
+#undef max
+
+#include <algorithm>
 #include <cassert>
+#include <vector>
 
 namespace {
     const int TARGET_Y_COORD = 8;
     const uint8_t VALUE_FOR_BLINK = 10;
+    const uint8_t SPEED_FOR_SHAKE = 3;
+    const char GAME_OVER_TEXT[] = "GAME OVER";
 }
 
 class GameOverTitle {
 public:
     GameOverTitle()
+        : charShifts(strlen(GAME_OVER_TEXT))
     {
+        std::fill(charShifts.begin(), charShifts.end(), 0);
     }
 
     void update()
     {
         if (y_coordinate < TARGET_Y_COORD) {
             y_coordinate += 1;
+        } else {
+            speedCount -= 1;
+            if (speedCount == 0) {
+                speedCount = SPEED_FOR_SHAKE;
+
+                const auto keepFirst = charShifts[0];
+                std::rotate(charShifts.rbegin(), charShifts.rbegin() + 1, charShifts.rend());
+                charShifts[0] = keepFirst;
+
+                if (changeUp) {
+                    if (charShifts[0] > -1) {
+                        charShifts[0] -= 1;
+                    } else {
+                        changeUp = false;
+                    }
+                } else {
+                    if (charShifts[0] < 1) {
+                        charShifts[0] += 1;
+                    } else {
+                        changeUp = true;
+                    }
+                }
+            }
         }
     }
 
@@ -28,12 +61,25 @@ public:
     {
         gb.display.setColor(Color::lightgreen);
         gb.display.setFontSize(2);
-        gb.display.setCursor(5, y_coordinate);
-        gb.display.println("GAME OVER");
+
+        auto count = 0;
+
+        for (const auto shift : charShifts) {
+            const char c = GAME_OVER_TEXT[count];
+
+            if (c != ' ' && c != '\0') {
+                gb.display.setCursor(5 + 8 * count, y_coordinate + shift);
+                gb.display.print(c);
+            }
+            count += 1;
+        }
     }
 
 private:
     uint8_t y_coordinate = 0;
+    std::vector<int8_t> charShifts;
+    bool changeUp = true;
+    bool speedCount = SPEED_FOR_SHAKE;
 };
 
 class RetryOrTitle {
